4b1a0a6d7beaff7351132dcb8a577134620f3907
Claire Sky <eclconnolly@gmail.com>
2021-12-12 13:01:31 -0800

    Employee test and object creation

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,17 @@
 const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Athena', '1234', 'athena@test.com');
+
+    expect(employee.name).toBe('Athena');
+    expect(employee.id).toBe('1234');
+    expect(employee.email).toBe('athena@test.com');
+});
+
+test('test retrival of name, id, email', () => {
+    const employee = new Employee('Athena', '1234', 'athena@test.com');
+
+    expect(employee.getName()).toBe('Athena');
+    expect(employee.getId()).toBe('1234');
+    expect(employee.getEmail()).toBe('athena@test.com');
+});
